82b842cea07b24d9335c76d31b09a4ece478b121
satalatt <sakari.latti@jyu.fi>
2015-04-07 18:02:59 +0300

    updated __init__.py

--- src/sdfconf/__init__.py
+++ src/sdfconf/__init__.py
@@ -1,1 +1,3 @@
 __all__ = [ 'functions', 'sdf', 'mol2', 'runner', 'findable' ]
+#import sdfconf.functions, sdfconf.sdf, sdfconf.mol2, sdfconf.runner, sdfconf.findable
+import functions, sdf, mol2, runner, findable
